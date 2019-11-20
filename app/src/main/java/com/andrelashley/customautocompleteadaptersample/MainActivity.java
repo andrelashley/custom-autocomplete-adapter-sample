bb36cadfee85569420f99055013bd5b7bba64af7
André Lashley <andre.lashley@gmail.com>
2019-11-20 11:07:24 -0800

    Add an autocomplete layout and implement initial activity setup

--- app/src/main/java/com/andrelashley/customautocompleteadaptersample/MainActivity.java
+++ app/src/main/java/com/andrelashley/customautocompleteadaptersample/MainActivity.java
@@ -3,12 +3,50 @@ package com.andrelashley.customautocompleteadaptersample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.andrelashley.customautocompleteadaptersample.models.Photo;
+import com.andrelashley.customautocompleteadaptersample.services.PhotoService;
+import com.andrelashley.customautocompleteadaptersample.services.ServiceBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout mProgressLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mProgressLayout = findViewById(R.id.progressLayout);
+        mProgressLayout.setVisibility(View.GONE);
+
+        fetchPhotos();
+
+    }
+
+    private void fetchPhotos() {
+        PhotoService photoService = ServiceBuilder.buildService(PhotoService.class);
+        Call<List<Photo>> request = photoService.getPhotos();
+
+        mProgressLayout.setVisibility(View.VISIBLE);
+        request.enqueue(new Callback<List<Photo>>() {
+            @Override
+            public void onResponse(Call<List<Photo>> call, Response<List<Photo>> response) {
+                mProgressLayout.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onFailure(Call<List<Photo>> call, Throwable t) {
+                mProgressLayout.setVisibility(View.GONE);
+            }
+        });
     }
 }
